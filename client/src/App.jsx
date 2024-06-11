0c03212ebf4b2febc591ff7f2394acc0363da069
Shivamk2745 <shivamkashyap0126@gmail.com>
2024-06-11 19:29:25 +0530

    Route Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
 const App = () => {
-  return <div className="text-4xl font-bold">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signin" element={<SignUp />}></Route>
+        <Route path="/project" element={<Project />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
